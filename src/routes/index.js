37356d2a4b31361d246c6cdfb592e5293ce17401
daosgava <daosgava.garcia@gmail.com>
2022-04-18 22:40:18 -0500

    change to  arrow functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res) {
+router.get("/", (req, res) => {
 	res.render("index", { face: "ಠωಠ", title: "Node Express Boilerplate" });
 });
 
